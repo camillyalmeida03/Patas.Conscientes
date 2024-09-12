cb6902f937620862dcc46c6dddf38b10769481b3
Maria Fernanda da Silva <168732466+MariaFernandaDaSilva@users.noreply.github.com>
2024-09-12 12:12:06 -0300

    Add files via upload

---
+++ js/adotarpg.js
@@ -0,0 +1,6 @@
+// js da página adotar.html
+
+// na página adotar.html, ao clicar no card, aparece as informações dele
+
+// Seleciona o elemento adotarMiniCard
+// Seleciona todos os elementos .adotarMiniCard
