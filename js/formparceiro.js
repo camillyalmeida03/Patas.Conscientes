c1fb342e30fa816c9ee6325e318ded2a951c0d79
Maria Fernanda da Silva <168732466+MariaFernandaDaSilva@users.noreply.github.com>
2024-09-27 12:30:00 -0300

    Add files via upload

---
+++ js/formparceiro.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", paginacarregada);
+
+const formParceiro1 = document.getElementById("formParceiro1");
+const formParceiro2 = document.getElementById("formParceiro2");
+
