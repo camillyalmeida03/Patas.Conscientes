1eec251b756c5a21bd2a32d3db941be4ae9d7fd2
Maria Fernanda da Silva <168732466+MariaFernandaDaSilva@users.noreply.github.com>
2024-09-30 12:35:54 -0300

    Add files via upload

--- js/formparceiro.js
+++ js/formparceiro.js
@@ -1,5 +1,24 @@
 window.addEventListener("load", paginacarregada);
 
-const formParceiro1 = document.getElementById("formParceiro1");
-const formParceiro2 = document.getElementById("formParceiro2");
+function paginacarregada(){
 
+let formParceiro1 = document.getElementById("formParceiro1");
+let formParceiro2 = document.getElementById("formParceiro2");
+let continuarFormParc = document.getElementById("continuarFormParc");
+let cadastrarFormParceiro = document.getElementById("cadastrarFormParceiro");
+
+continuarFormParc.addEventListener("click", function(){
+    event.preventDefault();
+    
+    formParceiro1.style.display = 'none';
+    formParceiro2.style.display = 'flex';
+})
+
+cadastrarFormParceiro.addEventListener("click", function(){
+    event.preventDefault();
+
+    
+    formParceiro1.style.display = 'flex';
+    formParceiro2.style.display = 'none';
+})
+}
---
+++ js/login.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", paginacarregada);
+
+function paginacarregada(){
+
+    let entrarCard = document.getElementById("entrarCard");
+    let cadastroCard = document.getElementById("cadastroCard");
+    let acssCadastrar = document.getElementById("acssCadastrar");
+    let acssEntrar = document.getElementById("acssEntrar");
+
+    acssCadastrar.addEventListener("click", function(){
+        event.preventDefault();
+        entrarCard.style.display = 'none';
+        cadastroCard.style.display = 'flex';
+    })
+
+    acssEntrar.addEventListener("click", function(){
+        event.preventDefault();
+        entrarCard.style.display = 'flex';
+        cadastroCard.style.display = 'none';
+    })
+
+
+
+}
