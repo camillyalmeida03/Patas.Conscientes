4bb1bcf5fb97334677d99dffff7be1bf3849623f
Maria Fernanda da Silva <168732466+MariaFernandaDaSilva@users.noreply.github.com>
2024-10-08 08:40:10 -0300

    Add files via upload

--- js/adotarpg.js
+++ js/adotarpg.js
@@ -54,3 +54,52 @@ function limparRadioButtons() {
 
     }
 
+    document.querySelectorAll('.adotarMiniCard').forEach(adotarMiniCard => {
+        adotarMiniCard.addEventListener('click', function (e) {
+            e.stopPropagation(); // Impede o clique de propagar para o documento
+    
+            const expandircardPet = this.querySelector('.expandircardPet');
+    
+            // Verifique se expandircardPet existe
+            if (!expandircardPet) {
+                console.error('Elemento .expandircardPet não encontrado dentro de adotarMiniCard');
+                return; // Saia da função se o elemento não existir
+            }
+    
+            const adotarMiniCardRect = this.getBoundingClientRect();
+            const expandircardPetRect = expandircardPet.getBoundingClientRect();
+    
+            expandircardPet.style.display = 'flex';
+    
+            const spaceTop = adotarMiniCardRect.top;
+            const spaceBottom = window.innerHeight - adotarMiniCardRect.bottom;
+            const spaceLeft = adotarMiniCardRect.left;
+            const spaceRight = window.innerWidth - adotarMiniCardRect.right;
+    
+            if (spaceRight >= expandircardPetRect.width) {
+                expandircardPet.style.left = `${adotarMiniCardRect.right}px`;
+                expandircardPet.style.top = `${adotarMiniCardRect.top}px`;
+            } else if (spaceLeft >= expandircardPetRect.width) {
+                expandircardPet.style.left = `${adotarMiniCardRect.left - expandircardPetRect.width}px`;
+                expandircardPet.style.top = `${adotarMiniCardRect.top}px`;
+            } else if (spaceBottom >= expandircardPetRect.height) {
+                expandircardPet.style.top = `${adotarMiniCardRect.bottom}px`;
+                expandircardPet.style.left = `${adotarMiniCardRect.left}px`;
+            } else if (spaceTop >= expandircardPetRect.height) {
+                expandircardPet.style.top = `${adotarMiniCardRect.top - expandircardPetRect.height}px`;
+                expandircardPet.style.left = `${adotarMiniCardRect.left}px`;
+            }
+        });
+    });
+    
+    document.addEventListener('click', (e) => {
+        document.querySelectorAll('.expandircardPet').forEach(expandircardPet => {
+            if (expandircardPet.style.display === 'flex' && !expandircardPet.parentElement.contains(e.target)) {
+                expandircardPet.style.display = 'none';
+            }
+        });
+    });
+    
+
+
+
