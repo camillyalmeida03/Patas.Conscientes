d803f7806d37204de92d980c24c35de820e070c3
Camilly Almeida <125749729+camillyalmeida03@users.noreply.github.com>
2025-07-26 16:35:41 -0300

    Add files via upload

--- js/formularios/login.js
+++ js/formularios/login.js
@@ -1,79 +1,81 @@
-window.addEventListener("load", paginacarregada);
+// window.addEventListener("load", paginacarregada);
 
-function paginacarregada(){
+// function paginacarregada(){
 
-    let entrarCard = document.getElementById("entrarCard");
-    let cadastroCard = document.getElementById("cadastroCard");
-    let acssCadastrar = document.getElementById("acssCadastrar");
-    let acssEntrar = document.getElementById("acssEntrar");
+//     let entrarCard = document.getElementById("entrarCard");
+//     let cadastroCard = document.getElementById("cadastroCard");
+//     let acssCadastrar = document.getElementById("acssCadastrar");
+//     let acssEntrar = document.getElementById("acssEntrar");
 
-    acssCadastrar.addEventListener("click", function(){
-        event.preventDefault();
-        entrarCard.style.display = 'none';
-        cadastroCard.style.display = 'flex';
-    })
+//     acssCadastrar.addEventListener("click", function(){
+//         event.preventDefault();
+//         entrarCard.style.display = 'none';
+//         cadastroCard.style.display = 'flex';
+//     })
 
-    acssEntrar.addEventListener("click", function(){
-        event.preventDefault();
-        entrarCard.style.display = 'flex';
-        cadastroCard.style.display = 'none';
-    })
+//     acssEntrar.addEventListener("click", function(){
+//         event.preventDefault();
+//         entrarCard.style.display = 'flex';
+//         cadastroCard.style.display = 'none';
+//     })
 
 
 
-}
+// }
 
-// Validação do formulário
+// // Validação do formulário
   
-  document.getElementById('formEntrar').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário
+//   document.getElementById('formEntrar').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Evita o envio do formulário
   
-    const email = document.getElementById('email2').value;
-    const erro1 = document.getElementById('erro1');
+//     const email = document.getElementById('email2').value;
+//     const erro1 = document.getElementById('erro1');
   
-    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   
-    if (!regexEmail.test(email) || email.length <= 3) {
-        erro1.textContent = 'Por favor, insira um email válido.';
-    } else {
-        erro1.style.display = 'none'; 
-    }
-  });
+//     if (!regexEmail.test(email) || email.length <= 3) {
+//         erro1.textContent = 'Por favor, insira um email válido.';
+//     } else {
+//         erro1.style.display = 'none'; 
+//     }
+//   });
 
-  //Validação senha
-  document.getElementById('formEntrar').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário
+//   //Validação senha
+//   document.getElementById('formEntrar').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Evita o envio do formulário
   
-    const senhaEntrar = document.getElementById('senhaEntrar').value;
-    const erro2 = document.getElementById('erro2');
+//     const senhaEntrar = document.getElementById('senhaEntrar').value;
+//     const erro2 = document.getElementById('erro2');
   
-    if (senhaEntrar !== '12345') {
-        erro2.textContent = 'Por favor, insira uma senha válida.';
-    } else {
-        erro2.style.display = 'none'; 
-    }
-  });
+//     if (senhaEntrar !== '12345') {
+//         erro2.textContent = 'Por favor, insira uma senha válida.';
+//     } else {
+//         erro2.style.display = 'none'; 
+//     }
+//   });
   
-  //Botão confirmar
-  document.getElementById('formEntrar').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário
+//   //Botão confirmar
+//   document.getElementById('formEntrar').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Evita o envio do formulário
   
    
-    const email = document.getElementById('email2').value;
-    const senhaEntrar = document.getElementById('senhaEntrar').value;
+//     const email = document.getElementById('email2').value;
+//     const senhaEntrar = document.getElementById('senhaEntrar').value;
     
-    // Validação do e-mail: formato padrão
-    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     // Validação do e-mail: formato padrão
+//     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     
-    if (!regexEmail.test(email)) {
-      email.textContent = 'Por favor, insira um e-mail válido.';
-    } else if ( senhaEntrar !== '12345') {
-      senhaEntrar.textContent = 'Por favor, insira uma senha válida.';
-    }else {
-      // window.location.href = 'index.html';
-    }
-  }
-  );
+//     if (!regexEmail.test(email)) {
+//       email.textContent = 'Por favor, insira um e-mail válido.';
+//     } else if ( senhaEntrar !== '12345') {
+//       senhaEntrar.textContent = 'Por favor, insira uma senha válida.';
+//     }else {
+//       // window.location.href = 'index.html';
+//     }
+//   }
+//   );
+
+  /////////////////////////////////////////
 
 //  // Seleciona o botão e o campo de senha
 // const togglePasswordButton = document.getElementById("togglePassword");
