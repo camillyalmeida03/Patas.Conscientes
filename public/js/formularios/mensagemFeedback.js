2c0a910f557adccff59c6905538a0bf100a20e7d
Maria Fernanda da Silva <168732466+MariaFernandaDaSilva@users.noreply.github.com>
2025-10-01 12:21:08 -0300

    Add files via upload

--- public/js/formularios/mensagemFeedback.js
+++ public/js/formularios/mensagemFeedback.js
@@ -1,14 +1,40 @@
 // Este arquivo montaa uma padrão para as mensagens de feeback no site
-
+import { CriarElementos } from "../criarElementos";
 class MensagemFeedback{
-    constructor(mensagem, elemento){
+    constructor(mensagem, pai){
         this.mensagem = mensagem;
-        this.elementoHtml = elemento
+        // this.elementoHtml = elemento
+        this.criar = new CriarElementos();
+        this.pai = pai
     }
 
-    feedbackCerto(mensagem, elemento){
+    feedbackSucess(mensagem, pai){
+        this.divPai = this.criar.createElement(
+            "div",
+            ["feedback", "feedbackSucess"], 
+            null,
+            pai,
+            "feedback"
+        )
 
+        this.pFeedback = this.criar.createElement(
+            "p",
+            null,
+            mensagem,
+            this.divPai,
+            null
+        )
+
+        this.iconeFeedback = this.criar.createElement(
+            "div",
+            "iconeFeedback",
+            null,
+            this.divPai,
+            null
+        )
     }
 }
 
-let teste = document.getElementById("feedback");
+const teste = document.getElementById("feedback");
+const testinho = new MensagemFeedback()
+testinho.feedbackSucess("Deu certo", teste)
