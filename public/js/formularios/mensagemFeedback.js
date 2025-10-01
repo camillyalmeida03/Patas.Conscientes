f5e71a1a9a38d95f828ab23ae41988dac70beb5e
Maria Fernanda da Silva <mfsfernandasilva794@gmail.com>
2025-10-01 09:18:24 -0300

    Criando código de padrão feedback
    Incompleto

---
+++ public/js/formularios/mensagemFeedback.js
@@ -0,0 +1,14 @@
+// Este arquivo montaa uma padrão para as mensagens de feeback no site
+
+class MensagemFeedback{
+    constructor(mensagem, elemento){
+        this.mensagem = mensagem;
+        this.elementoHtml = elemento
+    }
+
+    feedbackCerto(mensagem, elemento){
+
+    }
+}
+
+let teste = document.getElementById("feedback");
