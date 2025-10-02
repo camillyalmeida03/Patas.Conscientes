e76905e45106a33ba1615a3354deda1a8f4e99aa
Maria Fernanda da Silva <mfsfernandasilva794@gmail.com>
2025-10-02 17:41:09 -0300

    Padrão de mensagem feedback feito

--- public/js/formularios/mensagemFeedback.js
+++ public/js/formularios/mensagemFeedback.js
@@ -1,29 +1,28 @@
 // Este arquivo montaa uma padrão para as mensagens de feeback no site
-import { CriarElementos } from "../criarElementos";
-class MensagemFeedback{
-    constructor(mensagem, pai){
+import { CriarElementos } from "../criarElementos.js";
+export class MensagemFeedback {
+    constructor(mensagem, pai) {
         this.mensagem = mensagem;
-        // this.elementoHtml = elemento
         this.criar = new CriarElementos();
-        this.pai = pai
+        this.pai = pai;
     }
 
-    feedbackSucess(mensagem, pai){
+    feedbackSucess() {
         this.divPai = this.criar.createElement(
             "div",
-            ["feedback", "feedbackSucess"], 
+            ["feedback", "feedbackSucess"],
             null,
-            pai,
+            this.pai,
             "feedback"
-        )
+        );
 
         this.pFeedback = this.criar.createElement(
             "p",
             null,
-            mensagem,
+            this.mensagem,
             this.divPai,
             null
-        )
+        );
 
         this.iconeFeedback = this.criar.createElement(
             "div",
@@ -31,10 +30,40 @@ class MensagemFeedback{
             null,
             this.divPai,
             null
-        )
+        );
+
+        setTimeout(() => {
+            this.divPai.remove();
+        }, 3000);
+    }
+
+    feedbackError() {
+        this.divPai = this.criar.createElement(
+            "div",
+            ["feedback", "feedbackError"],
+            null,
+            this.pai,
+            "feedback"
+        );
+
+        this.pFeedback = this.criar.createElement(
+            "p",
+            null,
+            this.mensagem,
+            this.divPai,
+            null
+        );
+
+        this.iconeFeedback = this.criar.createElement(
+            "div",
+            "iconeFeedback",
+            null,
+            this.divPai,
+            null
+        );
+
+        setTimeout(() => {
+            this.divPai.remove();
+        }, 3000);
     }
 }
-
-const teste = document.getElementById("feedback");
-const testinho = new MensagemFeedback()
-testinho.feedbackSucess("Deu certo", teste)
