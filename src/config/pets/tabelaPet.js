ad4366c7529abe65d5f2127a6142238d37a323bb
Maria Fernanda da Silva <mfsfernandasilva794@gmail.com>
2026-04-14 12:16:47 -0300

    Create tabelaPet.js

---
+++ src/config/pets/tabelaPet.js
@@ -0,0 +1,44 @@
+// Este arquivo é responsável por gerar uma linha para cada pet nas tabelas de gerenciamento das ONGs
+
+import { CriarElementos } from "../../../public/js/criarElementos.js";
+import { InformacoesPets } from "./informacoesPets.js";
+
+export class tabelaPets {
+    constructor(InformacoesPet = null) {
+        this.criarElemento = new CriarElementos();
+        this.InformacoesPet = new InformacoesPets();
+
+        if (InformacoesPet) {
+            this.InfoPet = InformacoesPet;
+            this.linhaTabPet = this.criarPetTabela(this.InfoPet);
+        }
+
+        this.informacoesExibidas = false;
+    }
+
+    criarPetTabela(InfoPet) {
+        const tbody = document.getElementById('tbodyTabelaPet');
+
+        if (tbody) {
+            this.trPet = this.criarElemento.createElement("tr", "trPet", null, tbody, null);
+            this.tdStatus = this.criarElemento.createElement("td", "tdStatus", "Em breve", this.trPet);
+            this.tdFoto = this.criarElemento.createElement("td", "tdFoto", "Em breve", this.trPet);
+            this.tdNome = this.criarElemento.createElement("td", "tdNome", InfoPet.nome, this.trPet);
+            this.tdEspecie = this.criarElemento.createElement("td", "tdEspecie", InfoPet.especie, this.trPet);
+            this.tdRaca = this.criarElemento.createElement("td", "tdRaca", InfoPet.raca, this.trPet);
+            this.tdSexo = this.criarElemento.createElement("td", "tdSexo", InfoPet.sexo, this.trPet);
+            this.tdPorte = this.criarElemento.createElement("td", "tdPorte", InfoPet.porte, this.trPet);
+            this.tdIdade = this.criarElemento.createElement("td", "tdIdade", InfoPet.idade, this.trPet);
+            this.tdPeso = this.criarElemento.createElement("td", "tdPeso", InfoPet.peso, this.trPet);
+            this.tdDesc = this.criarElemento.createElement("td", "tdDesc", InfoPet.sobre, this.trPet);
+            this.tdDtPost = this.criarElemento.createElement("td", "tdDtPost", "Em breve", this.trPet);
+            this.tdDtAtt = this.criarElemento.createElement("td", "tdDtAtt", "Em breve", this.trPet);
+            this.tdEditar = this.criarElemento.createElement("td", "tdEditar", null, this.trPet, "botaoEditarPet");
+            this.tdDeletar = this.criarElemento.createElement("td", "tdDeletar", null, this.trPet, "botaoDeletarPet");
+            this.botaoEditar = this.criarElemento.createButton("botaoTabela", "Editar", this.tdEditar, `Editar ${InfoPet.nome}`);
+            this.botaoDeletar = this.criarElemento.createButton("botaoTabela", "Excluir", this.tdDeletar, `Excluir ${InfoPet.nome}`);
+
+        }
+    }
+}
+
